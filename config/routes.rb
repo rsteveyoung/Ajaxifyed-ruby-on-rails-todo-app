702a268778d01fbbc3a91e6f24c9be29638cccdf
R. Steve Young <r.steve.young@gmail.com>
2018-01-01 13:38:34 -0300

    Setting up tasks resource and root route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  resources :tasks
 
-  get 'tasks/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
 end
